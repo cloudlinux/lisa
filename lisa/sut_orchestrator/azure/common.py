46bc22e7c2ba6da9900496db16fe25a7ce166bb7
Konstantin Olshanov <kolshanov@cloudlinux.com>
2023-11-12 11:13:53 +0400

    azure_sig: specify disk controller types for new image definitions
    Signed-off-by: Konstantin Olshanov <kolshanov@cloudlinux.com>

--- lisa/sut_orchestrator/azure/common.py
+++ lisa/sut_orchestrator/azure/common.py
@@ -2001,7 +2001,14 @@ def check_or_create_gallery_image(
                     "offer": gallery_image_offer,
                     "sku": gallery_image_sku,
                 },
+                "features": [
+                    {
+                        "name": "DiskControllerTypes",
+                        "value": "SCSI,NVMe",
+                    },
+                ],
             }
+
             if gallery_image_securitytype:
                 image_post_body["features"] = [
                     {
